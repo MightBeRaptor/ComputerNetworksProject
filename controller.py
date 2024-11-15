23e502a74b80b46c0d7409758db56509dcf8a5f5
MightBeRaptor <huntersecondemail@gmail.com>
2024-11-14 23:12:56 -0500

    updated login function

---
+++ controller.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from view import View, LoginView
+
+class Controller:
+    def __init__(self) -> None:
+        self.root = tk.Tk()
+        self.view = View(self.root, self)
+        self.loginView = LoginView(self.root, self)
+
+    def run(self) -> None:
+        self.root.title("Computer Networks Project")
+        self.root.geometry("600x350")
+        self.loginView.pack_widgets()
+        self.root.mainloop()
+
+    def login(self) -> None:
+        if self.loginView.usernameInput.get() == "username" and self.loginView.passwordInput.get() == "password":
+            messagebox.showinfo("Login Successful", "Welcome, Admin!")
+            self.loginView.unpack_widgets()
+            self.view.pack_widgets()
+        else:
+            messagebox.showerror("Login Failed", "Invalid username or password")
+
+if __name__ == "__main__":
+    c = Controller()
+    c.run()
