c37150c4ff9f6f54ebf13d7cb2d1457b0d67a639
Michael Mille <mmille183@gmail.com>
2024-11-26 17:10:58 -0500

    statistics file storage improved

--- network_analysis.py
+++ network_analysis.py
@@ -2,15 +2,18 @@ import pandas as pd
 import time
 import os
 
+FILE_STORAGE_PATH = "./file_storage"
+
 # function to obtain start time before file transfer
 def start_time():
     return time.time()
 
 # function to initially create dataframe
 def initialize_stats_dataframe():
-    if not os.path.exists("statistics_dataframe.csv"):
+    file_path = os.path.join(FILE_STORAGE_PATH, "statistics_dataframe.csv")
+    if not os.path.exists(file_path):
         columns = ["file_name", "file_size", "transfer_time", "data_rate"]
-        pd.DataFrame(columns=columns).to_csv("statistics_dataframe.csv", index=False)
+        pd.DataFrame(columns=columns).to_csv(file_path, index=False)
 
 # function to obtain and log the statistics for analysis in a .csv file
 def log_statistics(starting_time, file_path):
@@ -37,4 +40,5 @@ def log_statistics(starting_time, file_path):
 
     # appends new statistics into csv
     stats_df = pd.DataFrame([new_stats])
-    stats_df.to_csv("statistics_dataframe.csv", mode="a", header=not os.path.exists("statistics_dataframe.csv"),index=False)
+    stats_file_path = os.path.join(FILE_STORAGE_PATH, "statistics_dataframe.csv")
+    stats_df.to_csv(stats_file_path, mode="a", header=not os.path.exists(stats_file_path),index=False)
